5d0f7b35c8739742827876cb5ff4a135851245ea
Robert H <robert.hochstrat@gmail.com>
2021-12-22 21:28:23 +0100

    Day 22 pt01

--- src/AoC_Day22.py
+++ src/AoC_Day22.py
@@ -2,45 +2,168 @@ from lib import *
 from functools import reduce
 import time
 
-class Board:
-    def __init__(self, boardLines):
-        self.boardLines = []
-        self.LINES = len(boardLines)
-        self.COLUMNS = len(boardLines[0])
-        self.visited = []
-        for line in boardLines:
-            self.boardLines.append(list(map(int, list(line))))
-            #self.boardLines.append(list(line))
-            #self.boardLines.append(line)
+class Boat:
+    def __init__(self):
+        self.cubesOn = [[[False for i in range(-50, 51)] for j in range(-50, 51)] for k in range(-50, 51)]
 
-        def resetVisited(self):
-            self.costs = []
-            for i in range(self.LINES):
-                self.costs.append(self.COLUMNS * [False])
+    def doStep(self, s):
+        xFrom = max(-50, s[0])
+        xTo = min(50, s[1]) + 1
 
-        def inBoundsLine(self, iLine):
-            return 0 <= iLine < self.LINES
+        yFrom = max(-50, s[2])
+        yTo = min(50, s[3]) + 1
 
-        def inBoundsColumn(self, iColumn):
-            return 0 <= iColumn < self.COLUMNS
+        zFrom = max(-50, s[4])
+        zTo = min(50, s[5]) + 1
+
+        on = True if s[6] else False
+
+        for x in range(xFrom, xTo):
+            for y in range(yFrom, yTo):
+                for z in range(zFrom, zTo):
+                    self.cubesOn[x][y][z] = on
+
+    def numOn(self):
+        numOn = 0
+        for x in self.cubesOn:
+            for y in x:
+                numOn = numOn + y.count(True)
+        return numOn
+
+class Cubeoid:
+    def __init__(self, minX, maxX, minY, maxY, minZ, maxZ, on):
+        self.minX = minX
+        self.maxX = maxX
+        self.minY = minY
+        self.maxY = maxY
+        self.minZ = minZ
+        self.maxZ = maxZ
+        self.on = True if on == 1 else False
+
+class BootStep:
+    def __init__(self, cubeoids):
+        self.cubies = cubeoids
+
+    def calcOverlap(self, other):
+
+        res = []
+
+        for cuby in self.cubies:
+            for otherCuby in other.cubies:
+                cuby.calcIntersection(otherCuby, cuby.on, otherCuby.on)
+
+        return BootStep(res)
+
+    def doStep(self, s):
+        #todo: calc number of on cubes instead
+        xFrom = max(self.minX, s[0])
+        xTo = min(self.maxX, s[1]) + 1
+
+        yFrom = max(self.minY, s[2])
+        yTo = min(self.maxY, s[3]) + 1
+
+        zFrom = max(self.minZ, s[4])
+        zTo = min(self.maxZ, s[5]) + 1
+
+        on = True if s[6] else False
+
+        for x in range(xFrom, xTo):
+            for y in range(yFrom, yTo):
+                for z in range(zFrom, zTo):
+                    self.cubesOn[x][y][z] = on
+
 
 
 def part01(input01):
-    print("Todo")
-    return "todo"
+
+    bootSequence = [getAllNumbersFromString(i) for i in input01]
+    boat = Boat()
+
+    for i,s in enumerate(bootSequence):
+        if "on" in input01[i]:
+            s.append(1)
+        else:
+            s.append(0)
+        boat.doStep(s)
+
+    return boat.numOn()
 
 def part02(input02):
-    print("Todo")
-    return "todo"
+    bootSequence = [getAllNumbersFromString(i) for i in input02]
+
+    minX = 0
+    maxX = 0
+    minY = 0
+    maxY = 0
+    minZ = 0
+    maxZ = 0
+
+    bootSteps = []
+
+    for i, s in enumerate(bootSequence):
+        minX = min(s[0], minX)
+        maxX = max(s[1], maxX)
+
+        minY = min(s[2], minY)
+        maxY = max(s[3], maxY)
+
+        minZ = min(s[4], minZ)
+        maxZ = max(s[5], maxZ)
+
+        if "on" in input02[i]:
+            s.append(1)
+        else:
+            s.append(0)
+
+        bootSteps.append(BootStep([Cubeoid(minX, maxX, minY, maxY, minZ, maxZ, s[6])]))
+
+    print(reduce(lambda x, y: x.calcOverlap, bootSteps))
 
 
-pathDay = "day0X"
+    return 1
+
+
+class Cubeoid:
+    def __init__(self, minX,maxX, minY, maxY, minZ, maxZ):
+        self.minX = minX
+        self.maxX = maxX
+        self.minY = minY
+        self.maxY = maxY
+        self.minZ = minZ
+        self.maxZ = maxZ
+
+    def overlaps(self, other):
+
+        if self.minX <= other.minX <= self.maxX:
+            return True
+        if self.minY <= other.minY <= self.maxY:
+            return True
+        if self.minZ <= other.minZ <= self.maxZ:
+            return True
+        if self.minX <= other.maxX <= self.maxX:
+            return True
+        if self.minY <= other.maxY <= self.maxY:
+            return True
+        if self.minZ <= other.maxZ <= self.maxZ:
+            return True
+
+
+
+
+pathDay = "day22"
 testInput01 = readLinesFromFile("%s/input_test_01.txt" % pathDay)
 testResult01 = part01(testInput01)
-if testResult01 == "bullshit":
+if testResult01 == 39:
     print("Test Part 01 successful")
 else:
     print("Test Part 01 failed, returned", testResult01)
+
+testInput01 = readLinesFromFile("%s/input_test_01_2.txt" % pathDay)
+testResult01 = part01(testInput01)
+if testResult01 == 590784:
+    print("Test Part 01_1 successful")
+else:
+    print("Test Part 01_1 failed, returned", testResult01)
 
 testInput02 = readLinesFromFile("%s/input_test_02.txt" % pathDay)
 testResult02 = part02(testInput02)
@@ -57,6 +180,6 @@ print("Solution Part 01:", part01(inputAsArray))
 print("--- %s seconds ---" % (time.time() - start_time))
 
 start_time = time.time()
-print("Solution Part 02:", part02(inputAsArray))
+#print("Solution Part 02:", part02(inputAsArray))
 print("--- %s seconds ---" % (time.time() - start_time))
 
